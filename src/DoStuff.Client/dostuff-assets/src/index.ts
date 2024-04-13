99aa36787e2e3711fcff82389613f98c49583052
Kevin Jump <kevin@thejumps.co.uk>
2024-04-13 20:54:29 +0100

    index layout

--- src/DoStuff.Client/dostuff-assets/src/index.ts
+++ src/DoStuff.Client/dostuff-assets/src/index.ts
@@ -4,12 +4,9 @@ import { ManifestTypes } from '@umbraco-cms/backoffice/extension-registry';
 // load up the manifests here.
 import { manifests as dashboardManifests } from './dashboards/manifest.ts';
 
-const manifests: Array<ManifestTypes> = [
-    ...dashboardManifests
-];
+const manifests: Array<ManifestTypes> = [...dashboardManifests];
 
 export const onInit: UmbEntryPointOnInit = (_host, extensionRegistry) => {
-    
-    // register them here. 
-    extensionRegistry.registerMany(manifests);
+	// register the here.
+	extensionRegistry.registerMany(manifests);
 };
