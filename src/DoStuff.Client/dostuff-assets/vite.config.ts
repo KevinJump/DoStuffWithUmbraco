25f76570a13eefdb38779229e4337226a76c35e3
Kevin Jump <kevin@thejumps.co.uk>
2024-04-13 20:47:09 +0100

    vite config update

--- src/DoStuff.Client/dostuff-assets/vite.config.ts
+++ src/DoStuff.Client/dostuff-assets/vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    build: {
-        lib: {
-            entry: "src/index.ts", // your web component source file
-            formats: ["es"],
-        },
-        outDir: "../wwwroot/App_Plugins/DoStuff", 
-        emptyOutDir: true,
-        sourcemap: true,
-        rollupOptions: {
-            external: [/^@umbraco/],
-        },
-    },
+	build: {
+		lib: {
+			entry: 'src/index.ts',
+			formats: ['es'],
+		},
+		outDir: '../wwwroot/App_Plugins/DoStuff',
+		emptyOutDir: true,
+		sourcemap: true,
+		rollupOptions: {
+			external: [/^@umbraco/],
+		},
+	},
 });
